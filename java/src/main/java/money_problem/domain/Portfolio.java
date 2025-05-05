5e3bc21a0f6b36deee83608afc761a992bc63ba4
David Reber <david.reber13@gmail.com>
2025-05-05 14:14:26 +0200

    Add Bank field to Portfolio and add Positions

--- java/src/main/java/money_problem/domain/Portfolio.java
+++ java/src/main/java/money_problem/domain/Portfolio.java
@@ -1,11 +1,24 @@
 package money_problem.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Portfolio {
     
+    private List<Position> positions = new ArrayList<>();
+    private Bank bank;
+    
+    public Portfolio(Bank bank) {
+        this.bank = bank;
+    }
+
+
     public void add(Position position) {
+        positions.add(position);
     }
 
     public double getTotal(Currency currency) {
+        
         if(Currency.KRW.equals(currency)){
             return 2200;
         }
--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -4,12 +4,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
+import static money_problem.domain.Currency.EUR;
+import static money_problem.domain.Currency.USD;
+
 public class PortfolioTest {
     
     @Test
     @DisplayName("1.2 USD equals 1 EUR")
     void shouldConvertEuroCorrectly(){
-        var portfolio = new Portfolio();
+        Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+        var portfolio = new Portfolio(bank);
         portfolio.add(new Position(1.2, Currency.USD));
 
         double total = portfolio.getTotal(Currency.EUR);
@@ -20,7 +26,8 @@ public class PortfolioTest {
     @Test
     @DisplayName("10 EUR + 5 USD equal 17 USD")
     public void shouldAddEurosAndUsd(){
-        var portfolio = new Portfolio();
+        Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+        var portfolio = new Portfolio(bank);
         portfolio.add(new Position(10, Currency.EUR));
         portfolio.add(new Position(5, Currency.USD));
 
@@ -32,7 +39,8 @@ public class PortfolioTest {
     @Test
     @DisplayName("1100 KRW + 1 USD equal 2200 KRW")
     public void shouldAddKrwAndUsd(){
-        var portfolio = new Portfolio();
+        Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+        var portfolio = new Portfolio(bank);
         portfolio.add(new Position(1100, Currency.KRW));
         portfolio.add(new Position(1, Currency.USD));
 
