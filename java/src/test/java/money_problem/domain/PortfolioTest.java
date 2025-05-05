bbbb03018e54df52d9b03f311c292b57fef0fe4e
Kuerm <kuerm@gmx.net>
2025-05-05 14:06:34 +0200

    r use currencyConverter

--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -9,38 +9,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PortfolioTest {
 
+    private static final CurrencyConverter currencyConverter = CurrencyConverter.withExchangeRate(USD, EUR, 0.83);
+
     @Test
-    void whenCreatingEmptyPortfolioThenReturnZero() {
+    void whenCreatingEmptyPortfolioThenReturnZero() throws MissingExchangeRateException {
         Portfolio testee = new Portfolio();
 
-        double actual = testee.amount(Currency.USD);
+        double actual = testee.amount(Currency.USD, currencyConverter);
 
         assertThat(actual).isZero();
     }
 
     @Test
-    void addParticularAmountInUsdWhenPortfolioEmptyThenReturnExpectedAmount() {
+    void addParticularAmountInUsdWhenPortfolioEmptyThenReturnExpectedAmount() throws MissingExchangeRateException {
         Portfolio testee = new Portfolio();
 
         testee.add(1.0, Currency.USD);
 
-        double actual = testee.amount(Currency.USD);
+        double actual = testee.amount(Currency.USD, currencyConverter);
         assertThat(actual).isEqualTo(1.0);
     }
 
     @Test
-    void addParticularAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() {
+    void addParticularAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() throws MissingExchangeRateException {
         Portfolio testee = new Portfolio();
         testee.add(1.0, Currency.USD);
 
-        double actual = testee.amount(Currency.EUR);
+        double actual = testee.amount(Currency.EUR, currencyConverter);
 
         assertThat(actual).isCloseTo(0.83, Offset.offset(0.01));
     }
 
     @Test
     void addADifferentAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() throws MissingExchangeRateException {
-        CurrencyConverter currencyConverter = CurrencyConverter.withExchangeRate(USD, EUR, 0.83);
         Portfolio testee = new Portfolio();
 
         testee.add(2.0, Currency.USD);
