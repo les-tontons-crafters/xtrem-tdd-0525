563daf800f2f0ac9faa78bec0e440ac9e389c715
Marcel Mathis <maeseee@gmail.com>
2025-05-05 11:52:14 +0200

    Add portfolio

---
+++ java/src/test/java/money_problem/domain/Portfolio.java
@@ -0,0 +1,4 @@
+package money_problem.domain;
+
+public class Portfolio {
+}
---
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -0,0 +1,12 @@
+package money_problem.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class PortfolioTest {
+
+    @Test
+    void emptyPortfolio() {
+        Portfolio testee = new Portfolio();
+
+    }
+}
