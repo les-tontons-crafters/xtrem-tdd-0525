ca2b957377e6af6df9c978838f9af75da66763e8
Manuel <mantel1337@gmail.com>
2025-05-05 14:00:54 +0200

    add currency converter to portfolio amount

--- java/src/main/java/money_problem/domain/Portfolio.java
+++ java/src/main/java/money_problem/domain/Portfolio.java
@@ -1,5 +1,7 @@
 package money_problem.domain;
 
+import static money_problem.domain.Currency.USD;
+
 public class Portfolio {
 
     private double amount = 0;
@@ -13,6 +15,10 @@ public class Portfolio {
 
     public void add(double amount, Currency currency) {
         this.amount = amount;
+    }
+
+    public double amount(Currency to, CurrencyConverter currencyConverter) throws MissingExchangeRateException {
+        return currencyConverter.convert(amount, USD, to);
     }
 
 }
--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -3,51 +3,52 @@ package money_problem.domain;
 import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
+import static money_problem.domain.Currency.EUR;
+import static money_problem.domain.Currency.USD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PortfolioTest {
 
     @Test
     void whenCreatingEmptyPortfolioThenReturnZero() {
-        final Portfolio testee = new Portfolio();
+        Portfolio testee = new Portfolio();
 
-        final double actual = testee.amount(Currency.USD);
+        double actual = testee.amount(Currency.USD);
 
         assertThat(actual).isZero();
     }
 
     @Test
     void addParticularAmountInUsdWhenPortfolioEmptyThenReturnExpectedAmount() {
-        final Portfolio testee = new Portfolio();
+        Portfolio testee = new Portfolio();
 
         testee.add(1.0, Currency.USD);
 
-        final double actual = testee.amount(Currency.USD);
+        double actual = testee.amount(Currency.USD);
         assertThat(actual).isEqualTo(1.0);
     }
 
     @Test
     void addParticularAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() {
-        final Portfolio testee = new Portfolio();
+        Portfolio testee = new Portfolio();
         testee.add(1.0, Currency.USD);
 
-        final double actual = testee.amount(Currency.EUR);
-
+        double actual = testee.amount(Currency.EUR);
 
         assertThat(actual).isCloseTo(0.83, Offset.offset(0.01));
     }
 
     @Test
-    void addADifferentAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() {
-        final Portfolio testee = new Portfolio();
+    void addADifferentAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() throws MissingExchangeRateException {
+        CurrencyConverter currencyConverter = CurrencyConverter.withExchangeRate(USD, EUR, 0.83);
+        Portfolio testee = new Portfolio();
+
         testee.add(2.0, Currency.USD);
 
-        final double actual = testee.amount(Currency.EUR);
-
+        double actual = testee.amount(Currency.EUR, currencyConverter);
 
         assertThat(actual).isCloseTo(1.66, Offset.offset(0.01));
     }
-
 
 //5 USD + 10 EUR = 17 USD
 // 1 USD + 1100 KRW = 2200 KRW
