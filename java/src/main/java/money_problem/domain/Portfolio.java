ec5c8ace1f4d521b978019308ee5e1f40537b5a2
Manuel <mantel1337@gmail.com>
2025-05-05 14:55:50 +0200

    WIP :)

--- java/src/main/java/money_problem/domain/Portfolio.java
+++ java/src/main/java/money_problem/domain/Portfolio.java
@@ -3,28 +3,21 @@ package money_problem.domain;
 import java.util.HashMap;
 import java.util.Map;
 
-import static money_problem.domain.Currency.EUR;
 import static money_problem.domain.Currency.USD;
 
 public class Portfolio {
 
+    private final double amount = 0;
+
     Map<Currency, Double> currencyMap = new HashMap<>();
 
-    private double amount = 0;
-
-    public void add(final double amount, final Currency currency) {
-        if (currency.equals(EUR)) {
-            final double amountTemp = amount * 1.2;
-            final Double previousAmount = currencyMap.get(currency);
-            final Double currentAmount = amountTemp + previousAmount;
-            currencyMap.put(currency, currentAmount);
-            this.amount += amountTemp;
-            return;
-        }
-        this.amount += amount;
+    public void add(double amount, Currency currency) {
+        Double previousAmount = currencyMap.getOrDefault(currency, 0.0);
+        Double currentAmount = amount + previousAmount;
+        currencyMap.put(currency, currentAmount);
     }
 
-    public double amount(final Currency to, final CurrencyConverter currencyConverter) throws MissingExchangeRateException {
+    public double amount(Currency to, CurrencyConverter currencyConverter) throws MissingExchangeRateException {
         return currencyConverter.convert(amount, USD, to);
     }
 
