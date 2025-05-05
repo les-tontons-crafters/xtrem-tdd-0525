b35d8a8753e43275a8c95a381f4b10057f844176
Kuerm <kuerm@gmx.net>
2025-05-05 14:07:19 +0200

    d remove unused

--- java/src/main/java/money_problem/domain/Portfolio.java
+++ java/src/main/java/money_problem/domain/Portfolio.java
@@ -6,13 +6,6 @@ public class Portfolio {
 
     private double amount = 0;
 
-    public double amount(Currency currency) {
-        if (currency.equals(Currency.EUR)) {
-            return 0.83;
-        }
-        return amount;
-    }
-
     public void add(double amount, Currency currency) {
         this.amount = amount;
     }
