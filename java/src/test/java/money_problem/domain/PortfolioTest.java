b9efaf534aaf8e92c7945136008c51419cb6c7be
Marcel Mathis <maeseee@gmail.com>
2025-05-05 13:43:36 +0200

    Refactor Offset

--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -1,5 +1,6 @@
 package money_problem.domain;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,7 +34,7 @@ public class PortfolioTest {
         double actual = testee.amount(Currency.EUR);
 
 
-        assertThat(actual).isEqualTo(0.83);
+        assertThat(actual).isCloseTo(0.83, Offset.offset(0.01));
     }
 //5 USD + 10 EUR = 17 USD
 // 1 USD + 1100 KRW = 2200 KRW
