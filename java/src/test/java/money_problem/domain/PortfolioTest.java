babd60e0f197dd5725ba0e10a9c4e0f8ade42f46
Kuerm <kuerm@gmx.net>
2025-05-05 13:27:00 +0200

    r test

--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -1,6 +1,5 @@
 package money_problem.domain;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,17 +7,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PortfolioTest {
 
     @Test
-    @DisplayName("Checks empty Portfolio")
     void emptyPortfolio() {
         Portfolio testee = new Portfolio();
+
         double actual = testee.get(Currency.USD);
+
         assertThat(actual).isZero();
     }
 
     @Test
-    @DisplayName("Adds some money to Portfolio and reads it")
-    void addUSD() {
+    void addParticularAmountInUsdWhenPortfolioEmptyThenReturnExpectedAmount() {
         Portfolio testee = new Portfolio();
+
         testee.add(1.0, Currency.USD);
 
         double actual = testee.get(Currency.USD);
