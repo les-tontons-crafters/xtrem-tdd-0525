e23f96629352d8485e435cfee9648e300dcbd7a1
Kuerm <kuerm@gmx.net>
2025-05-05 13:33:59 +0200

    a addParticularAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur

--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -24,6 +24,17 @@ public class PortfolioTest {
         double actual = testee.amount(Currency.USD);
         assertThat(actual).isEqualTo(1.0);
     }
+
+    @Test
+    void addParticularAmountInUsdWhenPortfolioIsEmptyThenReturnExpectedAmountInEur() {
+        Portfolio testee = new Portfolio();
+        testee.add(1.0, Currency.USD);
+
+        double actual = testee.amount(Currency.EUR);
+
+
+        assertThat(actual).isEqualTo(0.82);
+    }
 //5 USD + 10 EUR = 17 USD
 // 1 USD + 1100 KRW = 2200 KRW
 }
