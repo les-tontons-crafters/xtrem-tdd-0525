19947d52de67f6121ae6aeb0340555dc6b6fa292
tr00d <guillaume.faas@live.fr>
2025-05-06 07:26:23 +0200

    refactor: move money operations on Position (Tell, Don't Ask)

--- java/src/main/java/money_problem/domain/Position.java
+++ java/src/main/java/money_problem/domain/Position.java
@@ -1,4 +1,15 @@
 package money_problem.domain;
 
 public record Position(double amount, Currency currency) {
+    public double add(double addedAmount) {
+        return amount() + addedAmount;
+    }
+
+    public double times(int times) {
+        return amount() * times;
+    }
+
+    public double divide(int divisor) {
+        return amount() / divisor;
+    }
 }
--- java/src/test/java/money_problem/domain/PositionTest.java
+++ java/src/test/java/money_problem/domain/PositionTest.java
@@ -6,25 +6,22 @@ import org.junit.jupiter.api.Test;
 import static money_problem.domain.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MoneyCalculatorTest {
+class PositionTest {
     @Test
     @DisplayName("5 USD + 10 USD = 15 USD")
     void shouldAddInUsd() {
-        assertThat(MoneyCalculator.add(new Position(5, USD), 10))
-                .isEqualTo(15);
+        assertThat(new Position(5, USD).add(10)).isEqualTo(15);
     }
 
     @Test
     @DisplayName("10 EUR x 2 = 20 EUR")
     void shouldMultiplyInEuros() {
-        assertThat(MoneyCalculator.times(new Position(10, EUR), 2))
-                .isEqualTo(20);
+        assertThat(new Position(10, EUR).times(2)).isEqualTo(20);
     }
 
     @Test
     @DisplayName("4002 KRW / 4 = 1000.5 KRW")
     void shouldDivideInKoreanWons() {
-        assertThat(MoneyCalculator.divide(new Position(4002, KRW), 4))
-                .isEqualTo(1000.5);
+        assertThat(new Position(4002, KRW).divide(4)).isEqualTo(1000.5);
     }
 }
