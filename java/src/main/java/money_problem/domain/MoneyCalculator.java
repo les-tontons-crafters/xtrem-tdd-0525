91609ee4eeab9d1e96283353faecae33cd73210d
FreduBaechler <fred23@gmx.ch>
2025-05-06 10:30:57 +0200

    introduce Money Object to MonyCalculator

--- java/src/main/java/money_problem/domain/MoneyCalculator.java
+++ java/src/main/java/money_problem/domain/MoneyCalculator.java
@@ -1,15 +1,15 @@
 package money_problem.domain;
 
 public class MoneyCalculator {
-    public static double add(double amount, Currency currency, double addedAmount) {
-        return amount + addedAmount;
+    public static double add(Money money, double addedAmount) {
+        return money.amount() + addedAmount;
     }
 
-    public static double times(double amount, Currency currency, int times) {
-        return amount * times;
+    public static double times(Money money, int times) {
+        return money.amount() * times;
     }
 
-    public static double divide(double amount, Currency currency, int divisor) {
-        return amount / divisor;
+    public static double divide(Money money, int divisor) {
+        return money.amount() / divisor;
     }
 }
