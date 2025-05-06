977623d471b7ea19bcef3909d59e8a3bdbd47bf7
David Reber <david.reber13@gmail.com>
2025-05-06 13:31:14 +0200

    refactor: adjust Test to convertNew

--- java/src/test/java/money_problem/domain/CurrencyConverterTest.java
+++ java/src/test/java/money_problem/domain/CurrencyConverterTest.java
@@ -5,44 +5,40 @@ import org.junit.jupiter.api.Test;
 
 import static money_problem.domain.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CurrencyConverterTest {
     private final CurrencyConverter currencyConverter = CurrencyConverter.withExchangeRate(new ExchangeRate(EUR, USD, 1.2));
 
     @Test
     @DisplayName("10 EUR -> USD = 12 USD")
-    void shouldConvertEuroToUsd() throws MissingExchangeRateException {
-        assertThat(currencyConverter.convert(new Position(10, EUR), USD))
-                .isEqualTo(new Position(12,USD));
+    void shouldConvertEuroToUsd()  {
         assertThat(currencyConverter.convertNew(new Position(10, EUR), USD))
                 .isEqualTo(new ConversionResult(new Position(12, USD)));
     }
 
     @Test
     @DisplayName("10 EUR -> EUR = 10 EUR")
-    void shouldConvertInSameCurrency() throws MissingExchangeRateException {
-        assertThat(currencyConverter.convert(new Position(10, EUR), EUR))
-                .isEqualTo(new Position(10, Currency.EUR));
+    void shouldConvertInSameCurrency()  {
+        assertThat(currencyConverter.convertNew(new Position(10, EUR), EUR))
+                .isEqualTo(new ConversionResult(new Position(10, Currency.EUR)));
     }
 
     @Test
     @DisplayName("Throws a MissingExchangeRateException in case of missing exchange rates")
     void shouldReturnALeftOnMissingExchangeRate() {
-        assertThatThrownBy(() -> currencyConverter.convert(new Position(10, EUR), KRW))
-                .isInstanceOf(MissingExchangeRateException.class)
-                .hasMessage("EUR->KRW");
+        assertThat(currencyConverter.convertNew(new Position(10, EUR), KRW).failure().getMessage())
+                .isEqualTo(new ConversionResult(new MissingExchangeRateException(EUR, KRW)).failure().getMessage());
     }
 
     @Test
     @DisplayName("Conversion with different exchange rates EUR to USD")
-    void shouldConvertWithDifferentExchangeRates() throws MissingExchangeRateException {
-        assertThat(currencyConverter.convert(new Position(10, EUR), USD))
-                .isEqualTo(new Position(12, USD));
+    void shouldConvertWithDifferentExchangeRates()  {
+        assertThat(currencyConverter.convertNew(new Position(10, EUR), USD))
+                .isEqualTo(new ConversionResult(new Position(12, USD)));
 
         currencyConverter.addExchangeRate(new ExchangeRate(EUR, USD, 1.3));
 
-        assertThat(currencyConverter.convert(new Position(10, EUR), USD))
-                .isEqualTo(new Position(13, USD));
+        assertThat(currencyConverter.convertNew(new Position(10, EUR), USD))
+                .isEqualTo(new ConversionResult(new Position(13, USD)));
     }
 }
