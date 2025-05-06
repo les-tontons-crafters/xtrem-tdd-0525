ab0ccd65c3693e2ed480d95c589e0cdaa3ffe869
Max <max.joel.mj@googlemail.com>
2025-05-06 10:57:51 +0200

    refactor moneycalculator methods to money

--- java/src/main/java/money_problem/domain/Money.java
+++ java/src/main/java/money_problem/domain/Money.java
@@ -1,4 +1,15 @@
 package money_problem.domain;
 
 public record Money(double amount, Currency currency) {
+	public static double add(Money money, double addedAmount) {
+	    return money.amount() + addedAmount;
+	}
+
+	public static double times(Money money, int times) {
+	    return money.amount() * times;
+	}
+
+	public static double divide(Money money, int divisor) {
+	    return money.amount() / divisor;
+	}
 }
--- java/src/test/java/money_problem/domain/MoneyTest.java
+++ java/src/test/java/money_problem/domain/MoneyTest.java
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test;
 import static money_problem.domain.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MoneyCalculatorTest {
+class MoneyTest {
     @Test
     @DisplayName("5 USD + 10 USD = 15 USD")
     void shouldAddInUsd() {
-        assertThat(MoneyCalculator.add(new Money(5, USD), 10))
+        assertThat(Money.add(new Money(5, USD), 10))
                 .isEqualTo(15);
     }
 
     @Test
     @DisplayName("10 EUR x 2 = 20 EUR")
     void shouldMultiplyInEuros() {
-        assertThat(MoneyCalculator.times(new Money(10, EUR), 2))
+        assertThat(Money.times(new Money(10, EUR), 2))
                 .isEqualTo(20);
     }
 
     @Test
     @DisplayName("4002 KRW / 4 = 1000.5 KRW")
     void shouldDivideInKoreanWons() {
-        assertThat(MoneyCalculator.divide(new Money(4002, KRW), 4))
+        assertThat(Money.divide(new Money(4002, KRW), 4))
                 .isEqualTo(1000.5);
     }
 }
