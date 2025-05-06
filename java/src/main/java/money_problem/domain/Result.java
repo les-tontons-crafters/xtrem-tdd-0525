9772107440a069156289789a9b5f0280ad98c683
tr00d <guillaume.faas@live.fr>
2025-05-06 14:50:04 +0200

    feat: add new Result class

---
+++ java/src/main/java/money_problem/domain/Result.java
@@ -0,0 +1,11 @@
+package money_problem.domain;
+
+public record Result<S, F>(S success, F failure) {
+    public static <S, F> Result<S, F> fromSuccess(S successValue) {
+        return new Result<>(successValue, null);
+    }
+
+    public static <S, F> Result<S, F> fromFailure(F failureValue) {
+        return new Result<>(null, failureValue);
+    }
+}
