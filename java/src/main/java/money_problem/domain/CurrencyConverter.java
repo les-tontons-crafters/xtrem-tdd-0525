b7231c53a76cf393728f04af36929082ecfea74a
tr00d <guillaume.faas@live.fr>
2025-05-06 11:11:37 +0200

    refactor: clean CurrencyConverter

--- java/src/main/java/money_problem/domain/CurrencyConverter.java
+++ java/src/main/java/money_problem/domain/CurrencyConverter.java
@@ -3,57 +3,39 @@ package money_problem.domain;
 import java.util.*;
 
 public final class CurrencyConverter {
-    private final Map<String, Double> exchangeRates;
     private final List<ExchangeRate> exchangeRatesNew = new ArrayList<>();
 
-    private CurrencyConverter(Map<String, Double> exchangeRates) {
-        this.exchangeRates = exchangeRates;
-    }
-
     public static CurrencyConverter withExchangeRate(ExchangeRate exchangeRate) {
-        var bank = new CurrencyConverter(new HashMap<>());
+        var bank = new CurrencyConverter();
         bank.addExchangeRate(new ExchangeRate(exchangeRate.from(), exchangeRate.to(), exchangeRate.rate()));
 
         return bank;
     }
 
     public void addExchangeRate(ExchangeRate exchangeRate) {
-        exchangeRates.put(keyFor(exchangeRate.from(), exchangeRate.to()), exchangeRate.rate());
-        Optional<ExchangeRate> exchangeRate1 = dummyName(exchangeRate.from(), exchangeRate.to());
+        Optional<ExchangeRate> exchangeRate1 = findExchangeRate(exchangeRate.from(), exchangeRate.to());
         exchangeRate1.ifPresent(exchangeRatesNew::remove);
         exchangeRatesNew.add(exchangeRate);
     }
 
-    private static String keyFor(Currency from, Currency to) {
-        return from + "->" + to;
-    }
-
     public double convert(Position position, Currency to) throws MissingExchangeRateException {
-        if (!canConvertNew(position.currency(), to)) {
+        if (!canConvert(position.currency(), to)) {
             throw new MissingExchangeRateException(position.currency(), to);
         }
-        return convertSafelyNew(position, to);
+        return convertSafely(position, to);
     }
 
-    private double convertSafelyNew(Position position, Currency to) {
+    private double convertSafely(Position position, Currency to) {
         return position.currency() == to
                 ? position.amount()
-                : position.amount() * retrieveRateForTargetCurrency(position.currency(), to);
+                : position.amount() * findExchangeRate(position.currency(), to).orElseThrow().rate();
     }
 
-    private double retrieveRateForTargetCurrency(Currency from, Currency to) {
-        return exchangeRatesNew.stream().filter(exchangeRate -> exchangeRate.from().equals(from) && exchangeRate.to().equals(to)).findFirst().orElseThrow().rate();
-    }
-
-    private Optional<ExchangeRate> dummyName(Currency from, Currency to) {
+    private Optional<ExchangeRate> findExchangeRate(Currency from, Currency to) {
         return exchangeRatesNew.stream().filter(exchangeRate -> exchangeRate.from().equals(from) && exchangeRate.to().equals(to)).findFirst();
     }
 
     private boolean canConvert(Currency from, Currency to) {
-        return from == to || exchangeRates.containsKey(keyFor(from, to));
-    }
-
-    private boolean canConvertNew(Currency from, Currency to) {
         return from == to || exchangeRatesNew.stream().anyMatch(exchangeRate -> exchangeRate.from().equals(from) && exchangeRate.to().equals(to));
     }
 }
