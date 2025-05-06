476900af3555633a114c3bc78449a8bdc93cb919
tr00d <guillaume.faas@live.fr>
2025-05-06 07:28:40 +0200

    refactor: move portfolio creation before each test

--- java/src/test/java/money_problem/domain/PortfolioTest.java
+++ java/src/test/java/money_problem/domain/PortfolioTest.java
@@ -2,6 +2,7 @@ package money_problem.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class PortfolioTest {
 
 	private static CurrencyConverter currencyConverter;
+	private Portfolio portfolio;
 
 	@BeforeAll
 	static void setup() {
@@ -19,39 +21,32 @@ class PortfolioTest {
 		currencyConverter.addExchangeRate(USD, KRW, 1100);
 	}
 
+	@BeforeEach
+	void BeforeEach() {
+		portfolio = new Portfolio(currencyConverter);
+	}
+
 	@Test
 	@DisplayName("1.2 USD = 1 EUR")
 	void getTotalInEur() throws MissingExchangeRatesException {
-		var portfolio = new Portfolio(currencyConverter);
 		portfolio.add(new Position(1.2, Currency.USD));
-
-		double total = portfolio.getTotal(Currency.EUR);
-
-		Assertions.assertThat(total).isEqualTo(1);
+		Assertions.assertThat(portfolio.getTotal(Currency.EUR)).isEqualTo(1);
 	}
 
 	@Test
 	@DisplayName("10 EUR + 5 USD = 17 USD")
 	void getTotalInUsd() throws MissingExchangeRatesException {
-		var portfolio = new Portfolio(currencyConverter);
 		portfolio.add(new Position(10, Currency.EUR));
 		portfolio.add(new Position(5, Currency.USD));
-
-		double total = portfolio.getTotal(Currency.USD);
-
-		Assertions.assertThat(total).isEqualTo(17.0);
+		Assertions.assertThat(portfolio.getTotal(Currency.USD)).isEqualTo(17.0);
 	}
 
 	@Test
 	@DisplayName("1100 KRW + 1 USD = 2200 KRW")
 	void getTotalInKrw() throws MissingExchangeRatesException {
-		var portfolio = new Portfolio(currencyConverter);
 		portfolio.add(new Position(1100, KRW));
 		portfolio.add(new Position(1, Currency.USD));
-
-		double total = portfolio.getTotal(KRW);
-
-		Assertions.assertThat(total).isEqualTo(2200.0);
+		Assertions.assertThat(portfolio.getTotal(KRW)).isEqualTo(2200.0);
 	}
 
 	@Test
@@ -59,10 +54,8 @@ class PortfolioTest {
 		var portfolio = new Portfolio(CurrencyConverter.withExchangeRate(KRW, USD, 1));
 		portfolio.add(new Position(1100, KRW));
 		portfolio.add(new Position(1, Currency.USD));
-
 		assertThatThrownBy(() -> portfolio.getTotal(EUR))
 				.isInstanceOf(MissingExchangeRatesException.class)
 				.hasMessage("KRW->EUR,USD->EUR");
 	}
-
 }
