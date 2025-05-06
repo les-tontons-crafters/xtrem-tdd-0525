194ac269a0803489403997c9661233bbb23dbc61
Max <max.joel.mj@googlemail.com>
2025-05-06 11:00:14 +0200

    remove money methods static

--- java/src/main/java/money_problem/domain/Money.java
+++ java/src/main/java/money_problem/domain/Money.java
@@ -1,15 +1,15 @@
 package money_problem.domain;
 
 public record Money(double amount, Currency currency) {
-	public static double add(Money money, double addedAmount) {
-	    return money.amount() + addedAmount;
+	public double add(double addedAmount) {
+	    return amount() + addedAmount;
 	}
 
-	public static double times(Money money, int times) {
-	    return money.amount() * times;
+	public double times(int times) {
+	    return amount() * times;
 	}
 
-	public static double divide(Money money, int divisor) {
-	    return money.amount() / divisor;
+	public double divide(int divisor) {
+	    return amount() / divisor;
 	}
 }
--- java/src/test/java/money_problem/domain/MoneyTest.java
+++ java/src/test/java/money_problem/domain/MoneyTest.java
@@ -10,21 +10,21 @@ class MoneyTest {
     @Test
     @DisplayName("5 USD + 10 USD = 15 USD")
     void shouldAddInUsd() {
-        assertThat(Money.add(new Money(5, USD), 10))
+        assertThat(new Money(5, USD).add(10))
                 .isEqualTo(15);
     }
 
     @Test
     @DisplayName("10 EUR x 2 = 20 EUR")
     void shouldMultiplyInEuros() {
-        assertThat(Money.times(new Money(10, EUR), 2))
+        assertThat(new Money(10, EUR).times(2))
                 .isEqualTo(20);
     }
 
     @Test
     @DisplayName("4002 KRW / 4 = 1000.5 KRW")
     void shouldDivideInKoreanWons() {
-        assertThat(Money.divide(new Money(4002, KRW), 4))
+        assertThat(new Money(4002, KRW).divide(4))
                 .isEqualTo(1000.5);
     }
 }
